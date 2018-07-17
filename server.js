0974ce67db19d5f4f782aad5f66001be29293611
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-07-17 11:24:12 -0500

    adds scrape route and article route

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var mongojs = require("mongojs");
 var request = require("request");
 var cheerio = require("cheerio");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+var axios = require("axios");
 
 // Initialize Express
 var app = express();
@@ -14,3 +17,65 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+var db = require("./models");
+
+var PORT = 3000;
+
+app.get("/scrape", function (req, res) {
+    // First, we grab the body of the html with request
+    axios.get("https://www.theonion.com/").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+
+        $("h1").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, send it to the client
+                    return res.json(err);
+                });
+        });
+        // If we were able to successfully scrape and save an Article, send a message to the client
+        res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
